fd6463c842077f980a8c9e466764679d9a8f38bb
brainbyte <jonohudson@gmail.com>
2022-10-10 09:25:57 +0100

    Initial Commit

---
+++ include/OgreConfigPaths.h
@@ -0,0 +1,8 @@
+#ifndef OGRECONFIGPATHS_H_
+#define OGRECONFIGPATHS_H_
+
+#define OGRE_CONFIG_DIR "D:/data/c-code/ogre/build/sdk/bin"
+#define OGRE_MEDIA_DIR "D:/data/c-code/ogre/build/sdk/Media"
+#define OGRE_PLUGIN_DIR "D:/data/c-code/ogre/build/sdk/bin" 
+
+#endif /* OGRECONFIGPATHS_H_ */
